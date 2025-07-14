8ebdde2ced8e73c348b461156f5538dc8ebe4013
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-14 10:46:43 +0530

    Added assignment

---
+++ Assignment/proof_of_work2.js
@@ -0,0 +1,17 @@
+import crypto from "crypto"
+
+function findHashWithPrefix(prefix) {
+    let input = 0
+    while (true) {
+        let inputStr = "100xdevs" + input.toString()
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex')
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash }
+        }
+        input++
+    }
+}
+
+const result = findHashWithPrefix("00000")
+console.log(`Input : ${result.input}`)
+console.log(`hash : ${result.hash}`)
